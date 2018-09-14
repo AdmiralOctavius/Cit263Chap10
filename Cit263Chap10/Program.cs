6a83bd7df70a99a41197a9aa1e9e463a014e34a4
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-09-14 12:48:55 -0400

    End of class 9/14
    I didn't catch the second half of class, will have to listen to today's lecture on my own time.

--- Cit263Chap10/Program.cs
+++ Cit263Chap10/Program.cs
@@ -10,12 +10,25 @@ namespace Cit263Chap10
     {
         static void Main(string[] args)
         {
-            string test = "Hello";
+            //string test = "Hello";
             SimpleTime test1 = new SimpleTime(12, 3, 12);
             SimpleTime test2 = new SimpleTime(11, 5, 2);
             Console.WriteLine(test2.ToString());
+
+
+            Vendor myMachine = new Vendor();
+            myMachine.SetupItem(0, 3.5, "Candy", 2);
+            myMachine.SetupItem(1, 4.5, "Communist manifesto", 23);
+            myMachine.SetupItem(2, 5, "Mismatch patty cake: the cake for kids", 4);
+            myMachine.SetupItem(3, 0.25, "Nintendo Switch", 1);
+            myMachine.SetupItem(4, 1, "Dollar bill", 5);
+            myMachine.SetupItem(5, 300, "Souls of the Damnned", 10);
+
+            myMachine.DisplayInventory();
         }
 
+
+       
        
     }
 
@@ -46,5 +59,81 @@ namespace Cit263Chap10
         string email;
         SimpleTime hiredate;
 
+    }
+
+    public class Item
+    {
+        private double price;
+        private string name;
+        private int quantity;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }//We want to allow the item to be changed after creation.
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }//We want to allow the item to be changed after creation.
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }//We want to allow the item to be changed after creation.
+        }
+
+    }
+
+    public class Vendor
+    {
+
+        private Item[] inventory;
+        public Vendor()
+        {
+                inventory = new Item[5];
+                //At the moment in addition to inializing the array in the constructor, we need to initalize the items as well.
+                //There should be a faster, less hard coded way to do this
+                inventory[0] = new Item();
+                inventory[1] = new Item();
+                inventory[2] = new Item();
+                inventory[3] = new Item();
+                inventory[4] = new Item();
+        }        
+
+        public void DisplayInventory()
+        {
+            int i=0;
+            foreach(Item item in inventory)
+            {
+                ++i;
+                Console.WriteLine(i + "] " + item.Name + " : " + item.Price);
+            }
+        }
+
+        public void SetupItem(int position, double inPrice, string inName, int inQuantity)
+        {
+            if (inventory.Length > position)
+            {
+                inventory[position].Name = inName;
+                inventory[position].Price = inPrice;
+                inventory[position].Quantity = inQuantity;
+                Console.WriteLine("Item: " + inventory[position].Name + " Added to machine");
+            }
+            else
+            {
+                Console.WriteLine("Too large of an index");
+            }
+        }
+
+        public void Shop()
+        {
+            int i = 0;
+            foreach (Item item in inventory)
+            {
+                ++i;
+                Console.WriteLine(i + "] " + item.Name + " : " + item.Price);
+            }
+        }
     }
 }
