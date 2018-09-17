0736a117ac956fd5691e91ab5cd7dc5c2101d6c8
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-09-17 13:51:58 -0400

    Nearer End
    Worked on the exceptions for user input, used TryParse method to check input and avoid exception. Need to add total calculation and checkout method in store function.

--- Cit263Chap10/Program.cs
+++ Cit263Chap10/Program.cs
@@ -13,7 +13,7 @@ namespace Cit263Chap10
             //string test = "Hello";
             SimpleTime test1 = new SimpleTime(12, 3, 12);
             SimpleTime test2 = new SimpleTime(11, 5, 2);
-            Console.WriteLine(test2.ToString());
+            //Console.WriteLine(test2.ToString());
 
 
             Vendor myMachine = new Vendor();
@@ -24,7 +24,7 @@ namespace Cit263Chap10
             myMachine.SetupItem(4, 1, "Dollar bill", 5);
             myMachine.SetupItem(5, 300, "Souls of the Damnned", 10);
 
-            myMachine.DisplayInventory();
+           // myMachine.DisplayInventory();
 
             myMachine.Shop();
         }
@@ -133,24 +133,61 @@ namespace Cit263Chap10
             int i = 0;
             int userIn = -1;
             int currentSelection = -1;
+            bool getNum = false;
+            //double currentTotal = 0;
+            //Instead of double use Item array
+            int[] totalQty = new int[5];
+
             foreach (Item item in inventory)
             {
                 ++i;
                 Console.WriteLine(i + "] " + item.Name + " : " + item.Price);
             }
-            Console.WriteLine("Please use the number keys to select your item. 1-5 for items, 0 to exit.");
-            while(userIn != 0)
+            Console.WriteLine("Please use the number keys to select your item. 1-5 for items, 6 to exit, 7 to check inventory.");
+            while(userIn != 5)
             {
-                userIn = int.Parse(Console.ReadLine());
-                if(userIn > 0 && userIn <= 5)
+                //userIn = int.Parse(Console.ReadLine());
+                getNum = Int32.TryParse(Console.ReadLine(), out userIn);
+                userIn = userIn -1;
+                if(userIn >= 0 && userIn <= 4)
                 {
-                    Console.WriteLine(inventory[userIn].Name + " costs: " + inventory[userIn].Price + ". There are: " + inventory[userIn].Quantity + " within this machine. Would you like to buy some? 1 for yes, 2 for no.");
+                    Console.WriteLine(inventory[userIn].Name + " costs: " + inventory[userIn].Price + ". There are: " + inventory[userIn].Quantity + " within this machine. Would you like to buy some? 1 for yes, preses any other number to see the menu.");
                     currentSelection = userIn;
-                    userIn = int.Parse(Console.ReadLine());
-                    if(userIn == 1)
+                    //userIn = int.Parse(Console.ReadLine());
+                    getNum = Int32.TryParse(Console.ReadLine(), out userIn);
+                    if (userIn == 1)
                     {
-
+                        Console.WriteLine("How many of this item do you want?");
+                        //userIn = int.Parse(Console.ReadLine());
+                        getNum = Int32.TryParse(Console.ReadLine(), out userIn);
+                        if (userIn > 0 && userIn <= inventory[currentSelection].Quantity)
+                        {
+                            Console.WriteLine("Okay, adding: " + userIn + " to your total.");                  
+                            totalQty[currentSelection] = userIn;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not a correct number");
+                        }
                     }
+                 
+                    Console.WriteLine("Returning to main menu...");
+                    i = 0;
+                    foreach (Item item in inventory)
+                    {
+                        ++i;
+                        Console.WriteLine(i + "] " + item.Name + " : " + item.Price);
+                    }
+                    Console.WriteLine("Please use the number keys to select your item. 1-5 for items, 6 to exit.");
+                 
+                }
+                else if(userIn == 5)
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid item number silly!");
                 }
             
             }
